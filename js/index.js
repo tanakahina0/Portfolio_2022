9c80c324d82867c7b16a51f2cdc3bf31fe00e627
tanakahina0 <108250078+tanakahina0@users.noreply.github.com>
2023-03-25 23:09:30 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -44,3 +44,30 @@ $('.slider').slick({
   variableWidth: true,//幅の違う画像の高さを揃えて表示
   dots: true,//下部ドットナビゲーションの表示
 });
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 400){//上から400pxスクロールしたら
+    $('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+    $('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+  }else{
+    if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+      $('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+      $('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+    }
+  }
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top a').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 700);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
